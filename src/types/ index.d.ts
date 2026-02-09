326fdd731891457d7102c04ab35c6c1734f020f3
Pratik Narule <pratiknarule88@gmail.com>
2026-02-10 01:26:44 +0530

    Added password enecrpyted field in prisma orm

---
+++ src/types/ index.d.ts
@@ -0,0 +1,18 @@
+
+import { Role } from "@prisma/client";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        role: Role;
+      };
+    }
+  }
+}
+
+export {};
+
+
+
