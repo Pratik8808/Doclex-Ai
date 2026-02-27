e82bf6eeac7440f8c575df59ee5f81073a97ef9b
Pratik Narule <pratiknarule88@gmail.com>
2026-02-27 23:41:01 +0530

    restarted

--- src/controllers/ auth.controller.ts
+++ src/controllers/ auth.controller.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from "express";
 import { registerUser, loginUser } from "../services/auth.service";
 
-export const register = async (req: Request, res: Response) => {
-  try {
-    const { email, password ,role} = req.body;
-    const user = await registerUser(email, password,role);
-    res.status(201).json(user);
-  } catch (err: any) {
-    res.status(400).json({ error: err.message });
-  }
-};
+  export const register = async (req: Request, res: Response) => {
+    try {
+      const { email, password ,role, firstName,
+    lastName  } = req.body;
+      const user = await registerUser(email, password,role,firstName,lastName);
+      res.status(201).json(user);
+    } catch (err: any) {
+      res.status(400).json({ error: err.message });
+    }
+  };
 
 export const login = async (req: Request, res: Response) => {
   try {
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,36 +4,76 @@ import { prisma } from "../prisma";
 import { Role } from "@prisma/client";
 
 const JWT_SECRET = process.env.JWT_SECRET || "dev_secret";
+//  
 
-export const registerUser = async (email: string, password: string,role:string) => {
+export const registerUser = async (email: string, password: string,role:string,firstName:string,lastName:string) => {
   const hashedPassword = await bcrypt.hash(password, 10);
     if (role !== Role.USER && role !== Role.LAWYER) {
     throw new Error("Invalid role selected");
   }
 
-  return prisma.user.create({
-    data: {
-      email,
-      role: role as Role,
-      password: hashedPassword,
-    },
-  });
+ return prisma.user.create({
+  data: {
+    email,
+    role: role as Role,
+    password: hashedPassword,
+    firstName,
+    lastName
+  },
+  select: {
+    id: true,
+    email: true,
+    firstName: true,
+    lastName: true,
+    role: true,
+    createdAt: true
+  }
+});
+
 };
 
 export const loginUser = async (email: string, password: string) => {
-  const user = await prisma.user.findUnique({ where: { email } });
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+
   if (!user) throw new Error("User not found");
 
   const isValid = await bcrypt.compare(password, user.password);
   if (!isValid) throw new Error("Invalid password");
 
-  const token = jwt.sign(
+
+  const accessToken = jwt.sign(
     { userId: user.id, role: user.role },
     JWT_SECRET,
-    { expiresIn: "1d" }
+    { expiresIn: "1h" }
   );
 
-  return { token };
+
+  const refreshToken = jwt.sign(
+    { userId: user.id },
+    process.env.REFRESH_SECRET!,
+    { expiresIn: "7d" }
+  );
+
+  // Save refresh token in DB
+  await prisma.user.update({
+    where: { id: user.id },
+    data: { refreshToken },
+  });
+
+  return {
+    accessToken,
+    refreshToken,
+    user: {
+      id: user.id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      role: user.role,
+    },
+  };
 };
+
 //testing2
 //testing
